2ed9735875be10c411126022ee9fa901e8545b27
tkddls8848 <tkddls8848@gmail.com>
2019-12-27 10:16:02 +0900

    test commit

---
+++ python.py
@@ -0,0 +1,31 @@
+# coding = UTF-8
+
+str = "ASDF"
+str1 = '''QWR
+ZXC
+'''
+print('\n*'*2)
+print(str+str1)
+print('*\n'*3)
+
+List = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+print(List[0])
+List[0] = 'ilyoil'
+print(List[0])
+print(List[0:3])
+Day = ['1', '2', '3', '4', '5']
+Cal = [List, Day]
+print(Cal)
+print(Cal[1][0])
+Day.append('Good Day!!')
+print(Cal)
+List.insert(1, '1')
+print(List)
+List.remove('1')
+print(List)
+List.sort()
+print(List)
+List.reverse()
+print(List)
+print(len(List))
